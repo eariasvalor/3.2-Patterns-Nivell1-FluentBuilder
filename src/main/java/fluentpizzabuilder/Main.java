8d1eadeee2372b3b9d82de3b6266735e3beaea84
Esther Arias <eariasvalor@gmail.com>
2025-11-06 20:42:11 +0100

    feat: instancing pizzaiolo and pizza objects

--- src/main/java/fluentpizzabuilder/Main.java
+++ src/main/java/fluentpizzabuilder/Main.java
@@ -5,11 +5,26 @@ import fluentpizzabuilder.enums.SizeType;
 
 public class Main {
     public static void main(String[] args){
-        BasePizzaBuilder<MargheritaBuilder> marguerita = new MargheritaBuilder();
-        marguerita
+        Pizzaiolo pizzaiolo = new Pizzaiolo();
+
+        MargheritaBuilder margheritaBuilder = new MargheritaBuilder();
+        ParmiggianaBuilder parmiggianaBuilder = new ParmiggianaBuilder();
+        ProsciuttoEFunghiBuilder prosciuttoEFunghiBuilder = new ProsciuttoEFunghiBuilder();
+
+        Pizza margherita = pizzaiolo.make(margheritaBuilder, SizeType.LARGE, DoughType.NEAPOLITAN);
+        Pizza parmiggiana = pizzaiolo.make(parmiggianaBuilder, SizeType.MEDIUM, DoughType.CLASSIC);
+        Pizza prosciuttoEFunghi = pizzaiolo.make(prosciuttoEFunghiBuilder, SizeType.SMALL, DoughType.GLUTENFREE);
+
+        Pizza customPizza = new MargheritaBuilder()
                 .size(SizeType.MEDIUM)
                 .dough(DoughType.NEAPOLITAN)
-                .build()
-                .toString();
+                .addDefaultToppings()
+                .add("Mushrooms")
+                .add("Olives")
+                .add("Bacon")
+                .build();
+
+        System.out.println(margherita);
+
     }
 }
